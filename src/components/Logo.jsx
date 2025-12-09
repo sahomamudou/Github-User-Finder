df15d59dff3e3dabb35a1844772bff7e2d6ad25b
sahomamudou <sahomamudou2003@gmail.com>
2025-12-09 13:25:16 +0000

    First commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import LogoWhite from "../assets/logo-white.svg";
 import LogoDark from "../assets/logo-dark.svg";
-
+// logo
 function Logo({ dark = false }) {
   return <img src={dark ? LogoWhite : LogoDark} alt="Logo" />;
 }
